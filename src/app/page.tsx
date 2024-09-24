6249efca58e2a1214076b73114577c4c5fb2cc42
vibhor <vibgitcode27@gmail.com>
2024-09-25 02:25:49 +0530

    added footer and some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,10 @@ import Navbar from "./components/navbar";
 import { BackgroundBeams } from "./components/hero";
 import { Button } from "./components/connectMetamask";
 import { motion } from "framer-motion";
-import meta from "../assets/MetaMask_Fox.svg-removebg-preview.png"; // Importing the image
+import { GithubOutlined , LinkedinOutlined , TwitterOutlined }  from "@ant-design/icons";
 import { World , GlobeConfig } from "./components/globe";
 import { Meteors } from "./components/cards";
+import { StickyScrollRevealDemo } from "./components/project_showcase";
 
 export default function Home() {
   const handleClick = () => {
@@ -39,6 +40,12 @@ const globeConfig = {
     autoRotateSpeed: 0.5,
   };
   const colors = ["#06b6d4", "#3b82f6", "#6366f1"];
+
+  const iconVariants = {
+    initial: { scale: 1, opacity: 0.6 },
+    hover: { scale: 1.2, opacity: 1 },
+  };
+
   const sampleArcs = [
     {
       order: 1,
@@ -404,32 +411,26 @@ const globeConfig = {
 
   return (
     <div className="mainDiv">
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="relative min-h-screen bg-black text-white">
         <BackgroundBeams className="absolute inset-0 z-0" />
         <div className="relative z-10 flex flex-col items-center justify-center h-screen">
           <div className="hero BigText text-center mb-8">
-            <h1 className="text mb-[-20px]">NextPay: Mutli EVM transactions</h1>
-            <h1 className="text">with zero Proofs</h1>
+            <h1 className="text mb-[-20px]">Ignite Your Passion for Technology</h1>
+            <h1>Join Mozilla Phoenix Club at JUET – Fueling innovation, creativity, and collaboration in the tech world</h1>
             <h1 className="text2 mt-4">
-              Seamless Ethereum transfers across multiple blockchains with anonymous Aadhaar-based KYC
+              Become part of a thriving community where technology meets creativity. Learn, build, and innovate with a like-minded team of tech enthusiasts.
             </h1>
           </div>
           <div className="flex justify-center items-center">
             <Button
-              onClick={handleClick} // Button click handler
-              duration={2000} // Duration of the border animation
-              containerClassName="custom-container-class" // Optional custom class for container
-              borderClassName="custom-border-class" // Optional custom class for border
+              onClick={handleClick}
+              duration={2000}
+              containerClassName="custom-container-class"
+              borderClassName="custom-border-class"
             >
               <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                <Image
-                  src={meta} // Using Image component from Next.js
-                  alt="MetaMask logo"
-                  width={30} // Specify width
-                  height={30} // Specify height
-                />
-                <span style={{ marginLeft: "8px", color: "white", fontSize: "20px", fontWeight: "600" }}>Connect</span>
+                <span style={{ marginLeft: "8px", color: "white", fontSize: "20px", fontWeight: "600" }}>Join the Revolution</span>
               </div>
             </Button>
           </div>
@@ -438,8 +439,8 @@ const globeConfig = {
 
       <section className="section bg-black text-white p-8 relative z-20 ">
         <div className="hero BigText text-center">
-          <h1 className="text-globe mb-4 z-20">Seamlessly Transfer Cryptos with Multilingual Audio Alerts</h1>
-          <h1 className="text2 mt-4 mb-0 z-20">Experience a revolutionary way to manage your digital assets with customizable sound notifications in multiple languages.</h1>
+          <h1 className="text-globe mb-4 z-20">Explore Our Global Tech Impact</h1>
+          <h1 className="text2 mt-4 mb-0 z-20">At Mozilla Phoenix Club JUET, we are deeply committed to the principles of open-source collaboration. Our GitHub Globe highlights the contributions made by our passionate members, connecting innovators across the world. Through code, knowledge sharing, and community-driven projects, we actively contribute to the global open-source ecosystem. Each pin on the globe represents a location where our members have made impactful contributions, furthering the open-source movement and building solutions that benefit everyone.</h1>
         </div>
 
         <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto bg-black relative w-full" style={{ marginTop : "-200px"}}>
@@ -467,11 +468,21 @@ const globeConfig = {
         </div>
       </section>
 
+      <section className="section bg-black text-white p-8 relative z-20 ">
+        <div className="hero BigText text-center">
+          <h1 className="text-globe mb-4 z-20">Project showcase</h1>
+        </div>
+      
+      <div>
+        <StickyScrollRevealDemo/>
+      </div>
+      </section>
+
       <section className="relative z-30 py-16 bg-black text-white mt-40">
         <div className="hero BigText text-center mb-20">
-          <h1 className="text-globe">How it works Step - by - Step Guide</h1>
-          <h1 className="text2 mt-4 mb-0">
-            Payu: Effortless multi-chain Ethereum transfers with secure KYC and real-time alerts
+          <h1 className="text-globe">Why Join Us</h1>
+          <h1 className="text2 mt-4 mb-0" style={{ fontSize : "20px"}}>
+            Becoming a part of Mozilla Phoenix Club offers more than just coding experience; it’s an opportunity to grow holistically. You’ll have access to:
           </h1>
         </div>
         <div style={{ display : "flex" , flexDirection : "row" , justifyContent : "space-evenly", alignContent : "center" , }}>
@@ -499,13 +510,11 @@ const globeConfig = {
                   </div>
         
                   <h1 className="font-bold text-xl text-white mb-4 relative z-50">
-                    Meteors because they&apos;re cool
+                    Real-world Projects
                   </h1>
         
                   <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-                    I don&apos;t know what to write so I&apos;ll just paste something
-                    cool here. One more sentence because lorem ipsum is just
-                    unacceptable. Won&apos;t ChatGPT the shit out of this.
+                    Work on practical projects that will bolster your resume and give you hands-on experience.<br/><br/>
                   </p>
         
                   <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
@@ -515,6 +524,7 @@ const globeConfig = {
                 </div>
               </div>
           </div>
+
           <div className="">
             <div className=" w-full relative max-w-xs">
               <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
@@ -537,13 +547,11 @@ const globeConfig = {
                   </div>
         
                   <h1 className="font-bold text-xl text-white mb-4 relative z-50">
-                    Meteors because they&apos;re cool
+                    Competitions and Hackathons:
                   </h1>
         
                   <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-                    I don&apos;t know what to write so I&apos;ll just paste something
-                    cool here. One more sentence because lorem ipsum is just
-                    unacceptable. Won&apos;t ChatGPT the shit out of this.
+                    Participate in events that challenge your coding, problem-solving, and teamwork skills.
                   </p>
         
                   <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
@@ -556,7 +564,7 @@ const globeConfig = {
 
 
           <div className="">
-                      <div className=" w-full relative max-w-xs">
+              <div className=" w-full relative max-w-xs">
                         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
                           <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
                             <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
@@ -577,24 +585,109 @@ const globeConfig = {
                             </div>
                   
                             <h1 className="font-bold text-xl text-white mb-4 relative z-50">
-                              Meteors because they&apos;re cool
+                              Career Growth:
                             </h1>
                   
                             <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-                              I don&apos;t know what to write so I&apos;ll just paste something
-                              cool here. One more sentence because lorem ipsum is just
-                              unacceptable. Won&apos;t ChatGPT the shit out of this.
+                              Our project-based approach ensures you're building skills that are directly applicable to the workplace, helping you secure top placements.
                             </p>
                   
                             <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
                               Explore
                             </button>
                             <Meteors number={20} />
-                          </div>
                         </div>
-                    </div>
+              </div>
+            </div>
         </div>
       </section>
+
+      
+
+<footer className="bg-black dark:bg-gray-900 w-1/1">
+    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+              <a href="https://flowbite.com/" className="flex items-center">
+                  <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Mozilla</span>
+              </a>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+              <div>
+                  <h2 className="mb-6 text-sm font-semibold uppercase text-white">Resources</h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                      <li className="mb-4">
+                          <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
+                      </li>
+                      <li>
+                          <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 className="mb-6 text-sm font-semibold uppercase text-white">Follow us</h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                      <li className="mb-4">
+                          <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                      </li>
+                      <li>
+                          <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 className="mb-6 text-sm font-semibold uppercase text-white">Legal</h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                      <li className="mb-4">
+                          <a href="#" className="hover:underline">Privacy Policy</a>
+                      </li>
+                      <li>
+                          <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+            </span>
+            <div className="flex mt-4 sm:justify-center sm:mt-0">
+                <a href="#" className="text-gray-500 hover:text-white">
+                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                          <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
+                      </svg>
+                    <span className="sr-only">Facebook page</span>
+                </a>
+                <a href="#" className="text-gray-500 hover:text-white ms-5">
+                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
+                          <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/>
+                      </svg>
+                    <span className="sr-only">Discord community</span>
+                </a>
+                <a href="#" className="text-gray-500 hover:text-white ms-5">
+                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
+                      <path fill-rule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clip-rule="evenodd"/>
+                  </svg>
+                    <span className="sr-only">Twitter page</span>
+                </a>
+                <a href="#" className="text-gray-500 hover:text-white ms-5">
+                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                          <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
+                    </svg>
+                    <span className="sr-only">GitHub account</span>
+                </a>
+                <a href="#" className="text-gray-500 hover:text-white ms-5">
+                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path fill-rule="evenodd" d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z" clip-rule="evenodd"/>
+                  </svg>
+                    <span className="sr-only">Dribbble account</span>
+                </a>
+            </div>
+        </div>
+      </div>
+  </footer>
     </div>
   );
 }
